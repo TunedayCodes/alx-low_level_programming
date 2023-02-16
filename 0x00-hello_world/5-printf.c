6799c0b08f69279787e3853593911d1046181974
TunedayCodes <fadipetunde5@gmail.com>
2023-02-16 06:33:50 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this main functions prints a given txt
+ *
+ * Retun: 0 when the function is succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
